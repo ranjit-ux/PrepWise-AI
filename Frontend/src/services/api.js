ae0399a713884fc3ab5fb4496567ffd62d2f227d
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2026-05-20 17:44:24 +0530

    Connect frontend to deployed backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+
+    baseURL: " https://hazily-accuracy-tavern.ngrok-free.dev/api",
 });
 
 // Attach token to every request
